6f8132f5d854fe103a5f0ff7b0618f38768318bd
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-21 00:30:01 +0200

    modified the MasslawStepFunctionCaseFilePipelineNodeHandler base class to generically obtain and cache the execution stage of the state-machine execution

--- cloud-logic/lambda/src/modules/lambda_handler_template_step_functions_lambda_node_case_file_pipiline/_masslaw_step_functions_case_file_pipeline_node_handler.py
+++ cloud-logic/lambda/src/modules/lambda_handler_template_step_functions_lambda_node_case_file_pipiline/_masslaw_step_functions_case_file_pipeline_node_handler.py
@@ -4,14 +4,14 @@ from src.modules.lambda_handler_template_step_functions_lambda_node import StepF
 class MasslawStepFunctionCaseFilePipelineNodeHandler(StepFunctionLambdaNodeHandler):
     def __init__(self, name=None, default_response=None, event_structure=None, ):
         StepFunctionLambdaNodeHandler.__init__(self, name=name, default_response=default_response, event_structure=event_structure)
-
         self._file_id = ''
+        self._execution_stage = ''
 
     def _handle_event(self):
         StepFunctionLambdaNodeHandler._handle_event(self)
         self._file_id = self._get_request_event().get('file_id')
+        self._execution_stage = self._get_request_event().get('stage', 'prod')
 
     def _handle_exception(self, exception: Exception):
         self._set_response_attribute(['success'], 'false')
-
         StepFunctionLambdaNodeHandler._handle_exception(self, exception)
