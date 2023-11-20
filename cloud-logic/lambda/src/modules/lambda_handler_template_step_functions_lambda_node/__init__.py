aee2868e6f0de376e84385209ce37cda27f948f7
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-21 00:31:41 +0200

    fixed a fatal typo in the lambda_handler_template_step_functions_lambda_node __init__.py file (that didn't come up at static inspection in the IDE)
    a missing path specifier in the import expression

--- cloud-logic/lambda/src/modules/lambda_handler_template_step_functions_lambda_node/__init__.py
+++ cloud-logic/lambda/src/modules/lambda_handler_template_step_functions_lambda_node/__init__.py
@@ -1,4 +1,4 @@
-from _step_functions_lambda_node_handler import StepFunctionLambdaNodeHandler
+from ._step_functions_lambda_node_handler import StepFunctionLambdaNodeHandler
 
 
 __all__ = ["StepFunctionLambdaNodeHandler"]
