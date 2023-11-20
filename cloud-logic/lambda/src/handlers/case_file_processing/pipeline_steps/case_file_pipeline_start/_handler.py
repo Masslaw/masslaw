0b3c01de42d7a07dc8e84fd83981ab22933388d5
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-21 00:35:25 +0200

    now resets the file processing data of a file when the pipeline is executed in 'dev' or 'test' stages

--- cloud-logic/lambda/src/handlers/case_file_processing/pipeline_steps/case_file_pipeline_start/_handler.py
+++ cloud-logic/lambda/src/handlers/case_file_processing/pipeline_steps/case_file_pipeline_start/_handler.py
@@ -9,7 +9,10 @@ class CaseFilePipelineStart(MasslawStepFunctionCaseFilePipelineNodeHandler):
         if not self.__file_instance.is_valid():
             self._set_response_attribute(['invalid'], 'true')
             return
-        if self.__file_instance.get_data_property(['processing', 'in_progress']) in ('true', True):
+        if self._execution_stage in ('dev', 'test'):
+            self.__file_instance.set_data_property(['processing'], None)  # reset the processing information of a file to have a full execution of the state machine (no skips)
+        processing_in_progress = self.__file_instance.get_data_property(['processing', 'in_progress']) in ('true', True)
+        if processing_in_progress:
             self._set_response_attribute(['invalid'], 'true')
             return
         self.__file_instance.set_data_property(['processing', 'stage_information', 'ProcessingFile', 'status'], 'InProgress')
