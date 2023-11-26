f43c961bb5c4b183f8433a6a301b858d31c7e0c1
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-26 21:13:36 +0200

    the strength of a connection is saved to the relation's data in the data inflation step

--- content-processing/mlcp/src/logic_layer/text_processing/named_entity_recognition/ner_processors/spacy_wrapper/_spacy_document_processing/_document_data_inflation/_document_relations_data_inflation.py
+++ content-processing/mlcp/src/logic_layer/text_processing/named_entity_recognition/ner_processors/spacy_wrapper/_spacy_document_processing/_document_data_inflation/_document_relations_data_inflation.py
@@ -5,6 +5,7 @@ from logic_layer.text_processing.named_entity_recognition.ner_processors.spacy_w
 
 def inflate_relations_data(relations: List[DocumentEntityRelation]):
     _collect_evidence_for_relations(relations)
+    _write_relations_strength_to_data(relations)
 
 
 def _collect_evidence_for_relations(relations: List[DocumentEntityRelation]):
@@ -13,3 +14,8 @@ def _collect_evidence_for_relations(relations: List[DocumentEntityRelation]):
         for relating_token in relation.relating_tokens:
             relation_evidence_sentences.add(relating_token.sent)
         relation.relation_data['evidences'] = [{'t': sentence.text, 's': sentence.start_char, 'e': sentence.end_char} for sentence in relation_evidence_sentences]
+
+
+def _write_relations_strength_to_data(relations: List[DocumentEntityRelation]):
+    for relation in relations:
+        relation.relation_data['strength'] = relation.relation_strength
