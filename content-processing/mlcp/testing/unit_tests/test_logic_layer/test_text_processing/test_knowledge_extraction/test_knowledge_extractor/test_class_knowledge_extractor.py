d48aba0c63d61baa6148c6ba7add1c982d07e93a
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-27 15:23:33 +0200

    created tests for the KnowledgeExtractor class

---
+++ content-processing/mlcp/testing/unit_tests/test_logic_layer/test_text_processing/test_knowledge_extraction/test_knowledge_extractor/test_class_knowledge_extractor.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import Mock
+
+from logic_layer.knowledge_record import KnowledgeRecord
+from logic_layer.text_processing.knowledge_extraction._knowledge_extractor import KnowledgeExtractor
+from logic_layer.text_structures.extracted_optical_text_structure import ExtractedOpticalTextDocument
+
+
+class TestClassKnowledgeExtractor(unittest.TestCase):
+
+    def test_text_processing(self):
+        mock_knowledge_record = KnowledgeRecord()
+        text_to_process = 'This is some text to process.'
+
+        class KnowledgeExtractorMock(KnowledgeExtractor):
+            def _process_text(self, text: str):
+                self._knowledge_record = mock_knowledge_record
+
+        knowledge_extractor_mock = KnowledgeExtractorMock([])
+        knowledge_extractor_mock.load_text(text_to_process)
+
+        self.assertEqual(knowledge_extractor_mock.get_record(), mock_knowledge_record)
+
+    def test_optical_text_document_processing(self):
+        mock_knowledge_record = KnowledgeRecord()
+        optical_text_document_to_process = ExtractedOpticalTextDocument([Mock()])
+
+        class KnowledgeExtractorMock(KnowledgeExtractor):
+            def _process_optical_text_document(self, document: ExtractedOpticalTextDocument):
+                self._knowledge_record = mock_knowledge_record
+
+        knowledge_extractor_mock = KnowledgeExtractorMock([])
+        knowledge_extractor_mock.load_optical_text_document(optical_text_document_to_process)
+
+        self.assertEqual(knowledge_extractor_mock.get_record(), mock_knowledge_record)
