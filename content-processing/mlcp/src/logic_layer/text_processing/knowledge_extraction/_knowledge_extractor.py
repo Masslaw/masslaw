6f61854377a48b23879d3ed8e8cdf4f7dc39c97d
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-27 18:36:37 +0200

    added a "load_file" function to the knowledge extractor class (which loads a given file and targets the output to the correct processing method

--- content-processing/mlcp/src/logic_layer/text_processing/knowledge_extraction/_knowledge_extractor.py
+++ content-processing/mlcp/src/logic_layer/text_processing/knowledge_extraction/_knowledge_extractor.py
@@ -4,6 +4,7 @@ from typing import List
 from mlcp.src.logic_layer.knowledge_record import KnowledgeRecord
 from mlcp.src.logic_layer.knowledge_record.data_merging import RecordMerger
 from mlcp.src.logic_layer.text_structures.extracted_optical_text_structure import ExtractedOpticalTextDocument
+from mlcp.src.shared_layer.file_system_utils import file_system_utils
 
 
 class KnowledgeExtractor:
@@ -24,6 +25,18 @@ class KnowledgeExtractor:
 
     def load_optical_text_document(self, document: ExtractedOpticalTextDocument):
         self._process_optical_text_document(document)
+
+    def load_file(self, file_path: str):
+        file_type = file_system_utils.get_file_type(file_path)
+        if file_type in ('txt', 'text'):
+            with open(file_path, 'r') as f:
+                self._process_text(f.read())
+                return
+        if file_type in ('xml',):
+            # to be implemented when the document structure loading functionality is implemented
+            # self._process_optical_text_document(...)
+            raise NotImplementedError
+        raise ValueError(f"Unsupported file type: {file_type}")
 
     @abstractmethod
     def _process_text(self, text: str):
