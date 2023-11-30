664a00ac1bd0b5d21c2330b673ffee45a960966d
Idan Shemesh <idan.shmsh@gmail.com>
2023-11-30 09:34:46 +0200

    so far the processing sequence is reset if the state machine runs in the "dev" stage - replaced it with an explicit parameter "force_rerun"

--- cloud-logic/lambda/src/handlers/case_file_processing/pipeline_steps/case_file_pipeline_start/_handler.py
+++ cloud-logic/lambda/src/handlers/case_file_processing/pipeline_steps/case_file_pipeline_start/_handler.py
@@ -9,7 +9,8 @@ class CaseFilePipelineStart(MasslawStepFunctionCaseFilePipelineNodeHandler):
         if not self.__file_instance.is_valid():
             self._set_response_attribute(['invalid'], 'true')
             return
-        if self._execution_stage in ('dev', 'test'):
+        rerun_processing = self._get_request_event().get('force_rerun', 'false')
+        if rerun_processing in ("true", True):
             self.__file_instance.set_data_property(['processing'], None)  # reset the processing information of a file to have a full execution of the state machine (no skips)
         processing_in_progress = self.__file_instance.get_data_property(['processing', 'in_progress']) in ('true', True)
         if processing_in_progress:
