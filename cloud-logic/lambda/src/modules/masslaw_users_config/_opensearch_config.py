29cccc91f8d8927f422993f911f8f72c23f66808
Idan Shemesh <idan.shmsh@gmail.com>
2024-04-19 16:54:25 +0300

    changed the opensearch endpoint url to the dual-stack endpoint (preventing issues)

--- cloud-logic/lambda/src/modules/masslaw_users_config/_opensearch_config.py
+++ cloud-logic/lambda/src/modules/masslaw_users_config/_opensearch_config.py
@@ -1,2 +1,2 @@
-MASSLAW_USERS_OPENSEARCH_ENDPOINT = 'https://search-masslaw-users-brdojm3nynultialk46p5c5ase.us-east-1.es.amazonaws.com'
+MASSLAW_USERS_OPENSEARCH_ENDPOINT = 'https://search-masslaw-users-brdojm3nynultialk46p5c5ase.aos.us-east-1.on.aws'
 MASSLAW_USERS_OPENSEARCH_INDEX_NAME = 'masslaw-users-search-index'
